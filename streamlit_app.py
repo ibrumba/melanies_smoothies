157894765f537d2253c90ce3a15c193073a7ff75
ibrumba <mba_ibrahim@yahoo.com>
2024-08-06 13:04:09 -0500

    Update streamlit_app.py
    Fixing bug: File "/mount/src/melanies_smoothies/streamlit_app.py", line 47, in <module>
        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=TRUE)

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ if ingredients_list:
         st.subheader(fruit_chosen + " Nutrition Information")
         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         #st.text(fruityvice_response)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=TRUE)
+        fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     st.write(ingredients_string)
 
